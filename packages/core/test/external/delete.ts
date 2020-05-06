f32b75ede621e393b0748f4a79d739f78ce1ebba
Mesqueeb <mesqueeb@gmail.com>
2020-05-06 12:50:31 +0900

    add test

--- packages/core/test/external/delete.ts
+++ packages/core/test/external/delete.ts
@@ -14,15 +14,29 @@ test('delete', async t => {
   t.deepEqual(trainerModule.data, undefined)
 })
 
-test('revert: delete', async t => {
+test('revert: delete (local → remote)', async t => {
   const { trainerModule } = createVueSyncInstance()
   t.deepEqual(trainerModule.data, { age: 10, name: 'Luca' })
 
   try {
-    await trainerModule.delete('remote', { onError: 'revert' }) // mocks error on delete for remote store mock
+    await trainerModule.delete('remote', { onError: 'revert', executionOrder: ['local', 'remote'] }) // mocks error on delete for remote store mock
   } catch (error) {
     t.truthy(error)
   }
 
   t.deepEqual(trainerModule.data, { age: 10, name: 'Luca' })
 })
+
+// todo: for this test to work we need to mock a data pool for the remote plugin mock
+// test('revert: delete (remote → local)', async t => {
+//   const { trainerModule } = createVueSyncInstance()
+//   t.deepEqual(trainerModule.data, { age: 10, name: 'Luca' })
+
+//   try {
+//     await trainerModule.delete('local', { onError: 'revert', executionOrder: ['remote', 'local'] }) // mocks error on delete for remote store mock
+//   } catch (error) {
+//     t.truthy(error)
+//   }
+
+//   t.deepEqual(trainerModule.data, { age: 10, name: 'Luca' })
+// })
