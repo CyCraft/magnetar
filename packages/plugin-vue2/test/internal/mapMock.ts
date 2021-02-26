41ef9724357120bbd46fcb2d827954fd8d91868a
Luca Ban <mesqueeb@gmail.com>
2021-02-26 12:29:58 +0900

    test: fix test

--- packages/plugin-vue2/test/internal/mapMock.ts
+++ packages/plugin-vue2/test/internal/mapMock.ts
@@ -12,16 +12,15 @@ test('map mock', async (t) => {
   t.deepEqual(pokedexModule.data.get('136'), pokedex(136))
   t.is(pokedexModule.data.has('136'), true)
   t.deepEqual(
-    pokedexModule.data.keys(),
-    pokedexGetAll().map((p) => p.id)
+    [...pokedexModule.data.keys()],
+    pokedexGetAll().map((p) => `${p.id}`)
   )
-  t.deepEqual(pokedexModule.data.values(), pokedexGetAll())
+  t.deepEqual([...pokedexModule.data.values()], pokedexGetAll())
   const ids: string[] = []
-  pokedexModule.data.forEach((p) => ids.push(p.id))
-  t.deepEqual(pokedexModule.data.keys(), ids)
+  pokedexModule.data.forEach((p) => ids.push(`${p.id}`))
+  t.deepEqual([...pokedexModule.data.keys()], ids)
   t.is(pokedexModule.data.size, 151)
   pokedexModule.data.clear()
   t.is(pokedexModule.data.size, 0)
   t.is([...pokedexModule.data.keys()].length, 0)
-  t.is(pokedexModule.data.size, 151)
 })
