371a23743668067266d96944be084e5ff19149e9
Luca Ban <mesqueeb@gmail.com>
2021-02-26 17:16:20 +0900

    chore: update dependencies

--- packages/dev-firestore/vite.config.ts
+++ packages/dev-firestore/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: { include: ['firebase/app', 'firebase/firestore'] },
-  plugins: [vue()]
+  plugins: [vue()],
 })
