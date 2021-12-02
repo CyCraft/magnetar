e181495d634b903728cddb1dec2013dbcc434335
Luca Ban <mesqueeb@users.noreply.github.com>
2021-12-02 12:21:05 +0900

    export utils from 'magnetar'

--- packages/magnetar/src/index.ts
+++ packages/magnetar/src/index.ts
@@ -1,4 +1,5 @@
 export * from '@magnetarjs/core'
+export * from '@magnetarjs/utils'
 
 export * as PluginFirestore from '@magnetarjs/plugin-firestore'
 export * as PluginSimpleStore from '@magnetarjs/plugin-simple-store'
