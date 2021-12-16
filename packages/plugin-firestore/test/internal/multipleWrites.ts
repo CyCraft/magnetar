eb223f7f74ddc2d44e0910d80a3f036814ef7176
Wayne Fisher <wayne@fisherlea.com>
2021-12-16 15:52:28 -0500

    Add a couple tests for checking the timing of merges.

--- packages/plugin-firestore/test/internal/multipleWrites.ts
+++ packages/plugin-firestore/test/internal/multipleWrites.ts
@@ -557,3 +557,159 @@ import { firestoreDeepEqual } from '../helpers/firestoreDeepEqual'
     await firestoreDeepEqual(t, testName, 'pokedex/1', undefined)
   })
 }
+{
+  const testName = 'timing of multiple writes (merge → merge) to the same record'
+  test(testName, async (t) => {
+    const { pokedexModule } = await createMagnetarInstance(testName, {
+      insertDocs: { 'pokedex/1': pokedex(1) },
+    })
+    await firestoreDeepEqual(t, testName, 'pokedex/1', pokedex(1))
+    t.deepEqual(pokedexModule.doc('1').data, pokedex(1))
+
+    const all: Promise<any>[] = []
+    const debounce = 1000
+    let complete = 0
+    let timeoutResolve:any = null
+    let timeoutReject:any = null
+
+    function testComplete () {
+      complete++
+      if (complete === (all.length - 1)) {
+        if (timeoutResolve) {
+          timeoutResolve()
+        }
+      }
+    }
+
+    // Give some time for previous mutations to be flushed.
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true)
+      }, 2000)
+    })
+
+    // Time the merges. Watch for early completion and merges that take too long.
+    all.push(new Promise((resolve, reject) => {
+      timeoutResolve = resolve
+      timeoutReject = reject
+
+      // Check for early completion
+      setTimeout(() => {
+        if (complete) {
+          reject(new Error(complete + ' tests completed before the debounce time'))
+        }
+      }, debounce)
+
+      // Check for transactions that take too long.
+      setTimeout(() => {
+        reject(new Error('timeout - tests completed: ' + complete))
+      }, debounce + 500)
+    }))
+
+    try {
+      all.push(pokedexModule.doc('1').merge({ base: { HP: 9000 } }).then(testComplete))
+      all.push(pokedexModule.doc('1').merge({ base: { Attack: 50 } }).then(testComplete))
+    } catch (error) {
+      t.fail(JSON.stringify(error))
+      if (timeoutReject) {
+        timeoutReject(error)
+      }
+    }
+
+    await Promise.all(all)
+
+    const expected = {
+      id: 1,
+      name: 'Bulbasaur',
+      type: ['Grass', 'Poison'],
+      base: {
+        'HP': 9000,
+        'Attack': 50,
+        'Defense': 49,
+        'SpAttack': 65,
+        'SpDefense': 65,
+        'Speed': 45,
+      },
+    }
+    t.deepEqual(pokedexModule.doc('1').data, expected as any)
+    await firestoreDeepEqual(t, testName, 'pokedex/1', expected as any)
+  })
+}
+{
+  const testName = 'timing of multiple writes with custom debounce (merge → merge) to the same record'
+  test(testName, async (t) => {
+    const { pokedexModule } = await createMagnetarInstance(testName, {
+      insertDocs: { 'pokedex/1': pokedex(1) },
+    })
+    await firestoreDeepEqual(t, testName, 'pokedex/1', pokedex(1))
+    t.deepEqual(pokedexModule.doc('1').data, pokedex(1))
+
+    const all: Promise<any>[] = []
+    const debounce = 5000
+    let complete = 0
+    let timeoutResolve:any = null
+    let timeoutReject:any = null
+
+    function testComplete () {
+      complete++
+      if (complete === (all.length - 1)) {
+        if (timeoutResolve) {
+          timeoutResolve()
+        }
+      }
+    }
+
+    // Give some time for previous mutations to be flushed.
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true)
+      }, 2000)
+    })
+
+    // Time the merges. Watch for early completion and merges that take too long.
+    all.push(new Promise((resolve, reject) => {
+      timeoutResolve = resolve
+      timeoutReject = reject
+
+      // Check for early completion
+      setTimeout(() => {
+        if (complete) {
+          reject(new Error(complete + ' tests completed before the debounce time'))
+        }
+      }, debounce)
+
+      // Check for transactions that take too long.
+      setTimeout(() => {
+        reject(new Error('timeout - tests completed: ' + complete))
+      }, debounce + 500)
+    }))
+
+    try {
+      all.push(pokedexModule.doc('1').merge({ base: { HP: 9000 } }, { syncDebounceMs: debounce }).then(testComplete))
+      all.push(pokedexModule.doc('1').merge({ base: { Attack: 50 } }, { syncDebounceMs: debounce }).then(testComplete))
+    } catch (error) {
+      t.fail(JSON.stringify(error))
+      if (timeoutReject) {
+        timeoutReject(error)
+      }
+    }
+
+    await Promise.all(all)
+
+    const expected = {
+      id: 1,
+      name: 'Bulbasaur',
+      type: ['Grass', 'Poison'],
+      base: {
+        'HP': 9000,
+        'Attack': 50,
+        'Defense': 49,
+        'SpAttack': 65,
+        'SpDefense': 65,
+        'Speed': 45,
+      },
+    }
+    t.deepEqual(pokedexModule.doc('1').data, expected as any)
+    await firestoreDeepEqual(t, testName, 'pokedex/1', expected as any)
+  })
+}
