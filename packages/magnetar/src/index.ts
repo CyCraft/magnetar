bcf24f904da1f7e71a34eb2b113603fdd3143ec7
Luca Ban <mesqueeb@users.noreply.github.com>
2022-02-15 17:48:17 +0900

    fix: don't export both vue plugins from magnetar

--- packages/magnetar/src/index.ts
+++ packages/magnetar/src/index.ts
@@ -3,5 +3,4 @@ export * from '@magnetarjs/utils'
 
 export * as PluginFirestore from '@magnetarjs/plugin-firestore'
 export * as PluginSimpleStore from '@magnetarjs/plugin-simple-store'
-export * as PluginVue2 from '@magnetarjs/plugin-vue2'
 export * as PluginVue3 from '@magnetarjs/plugin-vue3'
