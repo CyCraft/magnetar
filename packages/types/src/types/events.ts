ff455f50226775e73638d335fb862fbb8c8cb315
mesqueeb <mesqueeb@users.noreply.github.com>
2022-09-24 16:19:07 +0900

    fix: re-add missing types

--- packages/types/src/types/events.ts
+++ packages/types/src/types/events.ts
@@ -97,14 +97,3 @@ export type EventNameFnsMap = {
   error: EventFnError[]
   revert: EventFnRevert[]
 }
-
-export function getEventNameFnsMap(...onMaps: (EventNameFnMap | void)[]): EventNameFnsMap {
-  const _onMaps = onMaps.filter(Boolean) as EventNameFnMap[]
-  const result: EventNameFnsMap = {
-    before: _onMaps.flatMap((on) => on.before ?? []),
-    success: _onMaps.flatMap((on) => on.success ?? []),
-    error: _onMaps.flatMap((on) => on.error ?? []),
-    revert: _onMaps.flatMap((on) => on.revert ?? []),
-  }
-  return result
-}
