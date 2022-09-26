6373413ebc876b59eb786ea1ab01d2168682f6cb
mesqueeb <mesqueeb@users.noreply.github.com>
2022-09-26 17:53:54 +0900

    fix: change port and update scripts

--- packages/plugin-firestore/test/helpers/initFirebase.ts
+++ packages/plugin-firestore/test/helpers/initFirebase.ts
@@ -11,6 +11,6 @@ const config = {
 }
 const firebaseApp = initializeApp(config)
 const db = initializeFirestore(firebaseApp, { experimentalAutoDetectLongPolling: true })
-connectFirestoreEmulator(db, 'localhost', 8099)
+connectFirestoreEmulator(db, 'localhost', 8198)
 
 export { db }
