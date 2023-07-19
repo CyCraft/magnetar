3f68526e4a08d51fa1d098ed19913a1e40469709
mesqueeb <mesqueeb@users.noreply.github.com>
2023-07-19 11:18:56 +0900

    build: pipeline

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,4 @@
+import MagnetarTable from './components/MagnetarTable.vue'
+
+export * from './types'
+export { MagnetarTable }
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,8 +1,35 @@
 /* eslint-disable tree-shaking/no-side-effects-in-initialization */
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { defineConfig } from 'vite'
+import pkg from './package.json' assert { type: 'json' }
+
+const nameCamel = pkg.name
+const namePascal = nameCamel.replace(/(^\w|-\w)/g, (c) => c.replace('-', '').toUpperCase())
+const dependencies = Object.keys(pkg.dependencies || [])
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    minify: false,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: namePascal,
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', ...dependencies],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
