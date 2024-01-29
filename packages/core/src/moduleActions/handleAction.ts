f4a93e7e03cde8c95b5b04059c27f77d5380966e
mesqueeb <mesqueeb@users.noreply.github.com>
2024-01-30 06:25:19 +0900

    fix: typo

--- packages/core/src/moduleActions/handleAction.ts
+++ packages/core/src/moduleActions/handleAction.ts
@@ -100,6 +100,7 @@ export async function handleAction(args: {
       // we need to revert first, then throw the error later
       return error
     }
+    return
   }
   // handle and await each eventFn in sequence
   for (const fn of on.success) {
