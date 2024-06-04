3c4b457483c332c75e238ee04cdee480f5939dba
mesqueeb <mesqueeb@users.noreply.github.com>
2024-06-04 14:19:08 +0900

    test: add test

--- packages/plugin-firestore/test/internal/insert.test.ts
+++ packages/plugin-firestore/test/internal/insert.test.ts
@@ -37,3 +37,95 @@ import { firestoreDeepEqual } from '../helpers/firestoreDeepEqual.js'
     await firestoreDeepEqual(testName, `pokedex/8`, undefined as any)
   })
 }
+{
+  const testName = 'write: insert (document) → set ID via doc instance'
+  test(testName, async () => {
+    const { pokedexModule, magnetar } = await createMagnetarInstance(testName)
+    await firestoreDeepEqual(testName, `pokedex/7`, undefined)
+    await firestoreDeepEqual(testName, `pokedex/007`, undefined)
+    const payload = pokedex(7)
+    assert.deepEqual(pokedexModule.data.get('7'), undefined)
+    await pokedexModule.doc('007').insert(payload).catch((e: any) => assert.fail(e.message)) // prettier-ignore
+    // check data of references executed on
+    assert.deepEqual(pokedexModule.data.get('007'), payload)
+    // check data of new references
+    assert.deepEqual(pokedexModule.doc('007').data, payload)
+    assert.deepEqual(magnetar.doc('pokedex/007').data, payload)
+    assert.deepEqual(magnetar.collection('pokedex').doc('007').data, payload)
+    await firestoreDeepEqual(testName, `pokedex/007`, payload)
+  })
+}
+{
+  const testName = 'write: insert (document) → set ID via payload'
+  test(testName, async () => {
+    const { pokedexModule, magnetar } = await createMagnetarInstance(testName)
+    await firestoreDeepEqual(testName, `pokedex/7`, undefined)
+    await firestoreDeepEqual(testName, `pokedex/007`, undefined)
+
+    const payload = { ...pokedex(7), id: '007' } as any
+    assert.deepEqual(pokedexModule.data.get('7'), undefined)
+    assert.deepEqual(pokedexModule.data.get('007'), undefined)
+    await pokedexModule.insert(payload).catch((e: any) => assert.fail(e.message)) // prettier-ignore
+    // check data of references executed on
+    assert.deepEqual(pokedexModule.data.get('007'), payload)
+    // check data of new references
+    assert.deepEqual(pokedexModule.doc('007').data, payload)
+    assert.deepEqual(magnetar.doc('pokedex/007').data, payload)
+    assert.deepEqual(magnetar.collection('pokedex').doc('007').data, payload)
+    await firestoreDeepEqual(testName, `pokedex/007`, payload)
+  })
+}
+{
+  const testName = 'write: insert (document) → set ID via hook'
+  test(testName, async () => {
+    const { pokedexModule, magnetar } = await createMagnetarInstance(testName)
+    await firestoreDeepEqual(testName, `pokedex/7`, undefined)
+    await firestoreDeepEqual(testName, `pokedex/007`, undefined)
+
+    const payload = pokedex(7)
+    assert.deepEqual(pokedexModule.data.get('7'), undefined)
+    assert.deepEqual(pokedexModule.data.get('007'), undefined)
+    await pokedexModule
+      .insert(payload, {
+        modifyPayloadOn: {
+          insert: (_payload) => ({ ..._payload, id: '007' }) as any,
+        },
+      })
+      .catch((e: any) => assert.fail(e.message))
+    const newExpected = { ...payload, id: '007' } as any
+    // check data of references executed on
+    assert.deepEqual(pokedexModule.data.get('007'), newExpected)
+    // check data of new references
+    assert.deepEqual(pokedexModule.doc('007').data, newExpected)
+    assert.deepEqual(magnetar.doc('pokedex/007').data, newExpected)
+    assert.deepEqual(magnetar.collection('pokedex').doc('007').data, newExpected)
+    await firestoreDeepEqual(testName, `pokedex/007`, newExpected)
+  })
+}
+{
+  const testName = 'write: insert (collection) → random ID'
+  test(testName, async () => {
+    const { pokedexModule, magnetar } = await createMagnetarInstance(testName)
+    await firestoreDeepEqual(testName, `pokedex/7`, undefined)
+    await firestoreDeepEqual(testName, `pokedex/007`, undefined)
+    const payload = pokedex(7)
+
+    let moduleFromResult: DocInstance<PokedexEntry>
+    try {
+      moduleFromResult = await pokedexModule.insert(payload)
+    } catch (error) {
+      assert.fail(JSON.stringify(error))
+      return
+    }
+    const newId = moduleFromResult.id
+    // check data of reference returned
+    assert.deepEqual(moduleFromResult.data, payload)
+    // check data of references executed on
+    assert.deepEqual(pokedexModule.data.get(newId), payload)
+    // check data of new references
+    assert.deepEqual(magnetar.doc(`pokedex/${newId}`).data, payload)
+    assert.deepEqual(magnetar.collection('pokedex').doc(newId).data, payload)
+    assert.deepEqual(pokedexModule.doc(newId).data, payload)
+    await firestoreDeepEqual(testName, `pokedex/${newId}`, payload)
+  })
+}
