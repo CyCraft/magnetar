3cf3feaf7b6318cea7f2d8f904195e080dd35c14
Luca Ban <mesqueeb@users.noreply.github.com>
2025-02-11 09:32:17 +1300

    fix: styles

--- packages/dev-vue3-firestore/src/components/TodoApp.vue
+++ packages/dev-vue3-firestore/src/components/TodoApp.vue
@@ -80,12 +80,12 @@ function toggleItemDone(item: Item) {
 
 <style lang="sass" scoped>
 .todo-app
-  background: black
-  @media (prefers-color-scheme: light)
-    background: #f7f7f7
   padding: 1rem
   border-radius: 1rem
   margin: 0 1rem
+  background: black
+  @media (prefers-color-scheme: light)
+    background: #f7f7f7
   button
     border-radius: 0.3rem
     border: none
