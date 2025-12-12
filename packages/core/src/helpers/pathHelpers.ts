7e8111a7701835cff46b888c7bee4456f4e76302
Luca Ban <mesqueeb@users.noreply.github.com>
2025-12-12 14:43:32 +1300

    fix: extra fix for new behaviour

--- packages/core/src/helpers/pathHelpers.ts
+++ packages/core/src/helpers/pathHelpers.ts
@@ -1,29 +1,13 @@
-import type { WriteLock } from '@magnetarjs/types'
 import { isCollectionModule } from '@magnetarjs/utils'
 
 /**
  * Returns a tuple with `[CollectionPath, DocId]` if the `DocId` is `undefined` that means that the `modulePath` passed is a collection!
  */
 export function getCollectionPathDocIdEntry(
-  modulePath: string
+  modulePath: string,
 ): [CollectionPath: string, DocId: string | undefined] {
   if (isCollectionModule(modulePath)) return [modulePath, undefined]
   const collectionPath = modulePath.split('/').slice(0, -1).join('/') // prettier-ignore
   const docId = modulePath.split('/').slice(-1)[0]
   return [collectionPath, docId]
-}
-
-/**
- * Gets all WriteLock objects of a certain `collectionPath` from the `WriteLockMap`
- */
-export function getCollectionWriteLocks(
-  collectionPath: string,
-  writeLockMap: Map<string, WriteLock>
-): WriteLock[] {
-  return [...writeLockMap.entries()]
-    .filter(([modulePath]) => {
-      const [_collectionPath] = getCollectionPathDocIdEntry(modulePath)
-      return _collectionPath === collectionPath
-    })
-    .map(([modulePath, writeLock]) => writeLock)
 }
